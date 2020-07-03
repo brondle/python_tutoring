b010d1ceced1a87eb2c9326901e3f01fe2ef9ece
Brent Bailey <bb2906@nyu.edu>
2020-07-03 17:42:46 -0400

    in-class changes

--- session_2/recursion_exercises/array_traversal.py
+++ session_2/recursion_exercises/array_traversal.py
@@ -5,4 +5,10 @@ def recursive_print(arr):
         print(arr[0])
         #how could we do this without passing a new version of the array, using indexing instead?
         recursive_print(arr[1:])
-recursive_print(test_arr)
+
+def recursive_print_with_index(array, i):
+    if i > 0:
+        recursive_print_with_index(array, i -1)
+        print(array[i])
+
+recursive_print_with_index(test_arr, len(test_arr) - 1)
