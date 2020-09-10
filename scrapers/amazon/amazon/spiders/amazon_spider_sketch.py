c4b6985d7fd4a7d98dc53ec12c52e37235db196b
Brent Bailey <bb2906@nyu.edu>
2020-09-10 14:14:58 -0400

    add explanatory comments

--- scrapers/amazon/amazon/spiders/amazon_spider_sketch.py
+++ scrapers/amazon/amazon/spiders/amazon_spider_sketch.py
@@ -8,6 +8,7 @@ class AmazonScraper(scrapy.Spider):
     start_urls = ["https://www.amazon.com/Darkness-Biographical-Introduction-Joseph-Conrad-ebook/product-reviews/B000FC1C50/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews"]
 
     def parse(self, response):
+        #check for "next" page button
         nextpageurl = response.xpath(
             '//li[@class="a-last"]/a/@href').get()
         print('next page url: ', nextpageurl)
@@ -15,18 +16,32 @@ class AmazonScraper(scrapy.Spider):
             yield item
 
         if nextpageurl:
+            #adding next page to our stack of pages
             nextpage = response.urljoin(nextpageurl)
 #            print("Found url: {}".format(nextpage))
+            #recursively call this function on that page
             yield scrapy.Request(nextpage, callback=self.parse)
 
     def scrape(self, response):
+        #CODE FOR SCRAPING "ALL REVIEWS"
          for review in response.css('span.a-size-base.review-text'):
             # get the title from the nested <a> link object inside the <h3> object
             yield {
                 'text': review.css('span.review-text-content span::text').get()
             }
 
-
+        ##ORIGINAL CODE FOR MAIN BOOK PAGE
+#        for review in response.css('span.a-size-base.review-text'):
+#        # for review in response.css('span.a-size-base.review-text'):
+#            # get the title from the nested <a> link object inside the <h3> object
+#            yield {
+#                # 'rating': book.css('p::text').get(),
+#                'title': review.css('div.review-text-content span::text').get(),
+#                # 'price': book.css('div.product_price p.price_color::text').get()
+#                # 'title': review.css('div.review-text-content span::text').get(),
+#
+#            }
+#
         #set a filename to write the response to
 #        filename = 'amazon_review_heart.html'
 #        with open(filename, "wb") as f:
