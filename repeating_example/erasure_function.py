04df3f0ada690b373c7e57f6d83754f0c4a1d075
Demirjian <ademirji@mu54126.myfiosgateway.com>
2020-10-08 22:33:49 -0400

    updated erasure files

--- repeating_example/erasure_function.py
+++ repeating_example/erasure_function.py
@@ -2,9 +2,6 @@ from fp import FP
 from fp import r_punct_list
 # import find_phoneme_oop
 from fp import refactored_find_phonemes, find_phonemes_ngram
-# from Find_phoneme import phoneme_list
-# from find_phoneme import *
-# from repeating import *
 import repeating
 import time
 import nltk
@@ -13,18 +10,16 @@ from nltk.tokenize import WhitespaceTokenizer, sent_tokenize
 import re
 prondict = nltk.corpus.cmudict.dict()
 from nltk.tokenize import WhitespaceTokenizer, sent_tokenize
-
 f = open('artistStatements.txt')
 raw2 = f.read()
 sentence = sent_tokenize(raw2)
-num = -2
-phon_a = ['AH0', 'N']
-phon_b = ['AH0', 'L']
-phon_c = ['AW1', 'ER0']
 
 rain = 'the rain in spain falls mainly on the plain'
 rain_list = rain.split()
-erase_list = []
+new_string = 'this is my new string, it isnt long or short or flat or thin, it is just right'
+new_s_list = new_string.split()
+new_string = 'this is my new string, it isnt long or short or flat or thin, it is just right'
+new_s_list = new_string.split()
 
 def seq_of_sents(sentence, num_of_sents):
     dont_exceed = len(sentence)-num_of_sents
@@ -35,41 +30,52 @@ num = 20
 #randomly pick n sentences
 this_choice = seq_of_sents(sentence,num)
 
-def erase_words(word_list, all_but_n):
-    sent_length = len(word_list)
-    num_to_erase = sent_length - all_but_n
-    erase_list = random.sample(word_list, num_to_erase)
-    # print(sent_length,num_to_erase,'\n', erase_list)
-    for i in range(len(word_list)):
-        if word_list[i] in erase_list:
-            char_length = len(word_list[i])
-            em = ' '
-            word_list[i] = em*char_length
-        else:
-            word_list[i] = word_list[i]
-            # print(word_list[i])
-    erased_string = list_to_str(word_list)
-    return erased_string
-
 def list_to_str(input_seq):
    # Join all the strings in list
    final_str = ' '.join(input_seq)
    return final_str
 
-print('\n'*2)
-# for i in range(len(this_choice)):
-#     new_erase_list = this_choice[i].split()
-#     rn = random.randint(1,5)
-#     print(erase_words(new_erase_list,rn))
+def random_l(length, rn):
+    num = random.sample(range(length), rn)
+    return num
 
+def erase_words(word_list):
+    sent_length = len(word_list)
+    # num_to_erase = sent_length - all_but_n
+    min = round(sent_length / 2)
+    max = round(sent_length / 1.20)
+    rn = random.randint(min, max)
+    random_list = random_l(sent_length, rn)
+    # erase_list = random.sample(word_list, num_to_erase)
+    # print(sent_length,num_to_erase,'\n', erase_list)
+    for i in range(sent_length):
+        if i in random_list:
+            char_length = len(word_list[i])
+            em = ' '
+            word_list[i] = em*char_length
 
+    erased_string = list_to_str(word_list)
+    return erased_string
+
+# print(erase_words(new_s_list))
+# print(new_string)
+
+limit = 7
 def erase_sentence(list_of_strings):
     for i in range(len(list_of_strings)):
         new_erase_list = list_of_strings[i].split()
-        rn = random.randint(1, 5)
-        erasure = erase_words(new_erase_list, rn)
-        print(erase_words(new_erase_list, rn))
+        # print(len(new_erase_list))
+        if len(new_erase_list) > limit:
+            new_erase_list= new_erase_list[:limit]
+            # print(new_erase_list)
+        erasure = erase_words(new_erase_list)
+        ran = random.random()
+        if ran > .5:
+            print()
+        print(erasure)
     # return erasure
 
 
-print(erase_sentence(this_choice))
+# print(erase_sentence(this_choice))
+erase_sentence(this_choice)
+
